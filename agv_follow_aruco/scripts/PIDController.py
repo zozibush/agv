e8178780d4a9128fd310e0f0de8794ba9155a8eb
zozibush <zozibush88@gmail.com>
2024-06-03 22:45:20 +0900

    Add PIDController class for implementing a PID controller

---
+++ agv_follow_aruco/scripts/PIDController.py
@@ -0,0 +1,15 @@
+class PID:
+    def __init__(self, Kp, Ki, Kd):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.prev_error = 0
+        self.integral = 0
+
+    def compute(self, setpoint, actual_position):
+        error = setpoint - actual_position
+        self.integral += error
+        derivative = error - self.prev_error
+        output = self.Kp * error + self.Ki * self.integral + self.Kd * derivative
+        self.prev_error = error
+        return output
