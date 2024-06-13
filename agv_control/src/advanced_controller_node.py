afa3e0fb7bf868ddd938e3044b76f632a5447e93
zozibush <zozibush88@gmail.com>
2024-06-13 23:10:42 +0900

    Update AGVAdvancedController to include robot name, aruco id, and last direction

--- agv_control/src/advanced_controller_node.py
+++ agv_control/src/advanced_controller_node.py
@@ -10,23 +10,27 @@ from agv_control.msg import AdvancedControl
 from PIDController import PID
 from Detection import Detection
 import math
+from agv_control.srv import JsonPath
+import json
+import sys
 
 class AGVAdvancedController:
 
     def __init__(self):
 
-        self.robot_name = rospy.get_param("robot_name", "AGV_OTA1")
-
-        self.connect()
+        self.robot_name = sys.argv[1]
+        self.current_aruco_id = int(sys.argv[2])
+        self.last_direction = sys.argv[3]
+        rospy.loginfo(f"Robot name: {self.robot_name}, Current aruco id: {self.current_aruco_id}, Last direction: {self.last_direction}")
 
         self.type = -1 # -1 : stop, 1 : follow aruco, 2 : follow lane, 3 : turn
-        self.epsilon = 1e-1
+        self.epsilon = 5e-1
         self.pause = False
         self.path = None
 
         self.detection = Detection()
         self.pid_aruco_x = PID(0.1, 0.00001, 0.05)
-        self.pid_aruco_z = PID(0.0005, 0.0, 0.005)
+        self.pid_aruco_z = PID(0.05, 0.0, 0.005)
         self.pid_lane = PID(0.0005, 0.0, 0.005)
 
         self.default_speed = 0.2
@@ -34,7 +38,6 @@ class AGVAdvancedController:
         # aruco
         self.far_linear_x = 0.7
         self.find_aruco = None
-        self.current_aruco_id = 0
 
         # lane
         self.max_desire_linear_vel = 0.3 # m/s
@@ -52,12 +55,14 @@ class AGVAdvancedController:
         # rotate
         self.is_rotate = False
         self.rotate_duration = 5
-        self.rotate_angle = math.pi / 2 / self.rotate_duration
+        self.direction_map = {"north":0, "east":90, "south":180, "west":270}
 
         # forward
         self.is_forward = False
         self.forward_duration = 5
         self.forward_distance = 2.03 + 0.7 # 카메라 위치 + aruco 간의 거리
+
+        self.connect()
 
     def connect(self):
         # Publisher
@@ -67,6 +72,18 @@ class AGVAdvancedController:
         self.image_sub = rospy.Subscriber(f"/{self.robot_name}/camera/image_raw/compressed", CompressedImage, self.camera_callback)
         self.control_sub = rospy.Subscriber(f"/{self.robot_name}/advanced_control", AdvancedControl, self.advanced_control_callback)
         self.pause_sub = rospy.Subscriber(f"/{self.robot_name}/pause", Bool, self.pause_callback)
+
+        # Service
+        rospy.wait_for_service('/aruco_map_service')
+        self.aruco_map_service = rospy.ServiceProxy('/aruco_map_service', JsonPath)
+
+    def get_path(self, start_node, end_node):
+        try:
+            response = self.aruco_map_service(start_node, end_node)
+            return json.loads(response.json_path)
+        except rospy.ServiceException as e:
+            rospy.logerr("Service call failed: %s" % e)
+            return None
 
     def pause_callback(self, data):
         self.pause = data.data
@@ -95,13 +112,18 @@ class AGVAdvancedController:
             if self.start_time is None:
                 self.start_time = rospy.Time.now().to_sec()
 
-            if self.turn == "r": # right
-                self.move(0, -self.rotate_angle)
-            else: # left and backward
-                self.move(0, self.rotate_angle)
+            angle1 = self.direction_map[self.last_direction]
+            angle2 = self.direction_map[self.direction]
+            diff = angle1 - angle2
+            diff = diff % 360
+            if diff > 180:
+                diff -= 360
 
-            k = 2 if self.turn == "b" else 1
-            if rospy.Time.now().to_sec() - self.start_time >= self.rotate_duration * k:
+            rotate_angle = math.radians(diff) / self.rotate_duration
+            self.move(0, rotate_angle)
+
+            k = 2 if abs(diff) == 180 else 1
+            if rospy.Time.now().to_sec() - self.start_time >= self.rotate_duration * k or diff == 0:
                 self.is_rotate = False
                 self.type = 2 # follow lane
                 self.start_time = None
@@ -129,16 +151,18 @@ class AGVAdvancedController:
         except:
             print("Error conversion to CV2")
 
-        if not self.current_aruco_id in self.path.keys():
+        if self.current_aruco_id not in self.path.keys():
             self.type = -1
             self.path = None
             rospy.loginfo("End of path")
             return
 
-        lane_color = self.path[self.current_aruco_id]["lane_color"]
-        next_aruco_id = self.path[self.current_aruco_id]["next_aruco_id"]
-        self.turn = self.path[self.current_aruco_id]["turn"]
-
+        next_aruco_id = int(self.path[self.current_aruco_id][0])
+        lane_color = self.path[self.current_aruco_id][1]
+        self.direction = self.path[self.current_aruco_id][2]
+        self.arrive = self.path[self.current_aruco_id][3]
+        if self.arrive is None:
+            self.arrive = self.direction
 
         corners, ids, final_img = self.detection.detectMarkers(cv_image)
 
@@ -173,10 +197,10 @@ class AGVAdvancedController:
                 cmd_vel = Twist()
 
                 self.current_aruco_id = next_aruco_id
+                self.last_direction = self.arrive
                 self.is_forward = True
 
         elif self.type == 2: # follow lane
-            # lane_color에 따라 필터 변경하기
 
             if ids is not None and ids[0][0] == next_aruco_id:
                 self.type = 1
@@ -194,34 +218,35 @@ class AGVAdvancedController:
             cmd_vel.angular.z = angular_z
 
         elif self.type == 3: # turn
-            if self.turn == "s":
-                self.type = 2
-            else:
-                self.is_rotate = True
-            return
+            self.is_rotate = True
 
         if self.detection.draw_img:
-            cv2.putText(final_img, f"distance: {self.pid_aruco_x.last_error:.6f}", (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,0,255), 1)
-            cv2.putText(final_img, f"cmd: ({cmd_vel.linear.x:.6f},0,{cmd_vel.linear.z:.6f})", (10,40), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,0,255), 1)
+            cv2.putText(final_img, f"distance: {self.pid_aruco_x.last_error:.3f}", (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,0,255), 1)
+            cv2.putText(final_img, f"cmd: ({cmd_vel.linear.x:.3f},{cmd_vel.angular.z:.3f})", (10,40), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,0,255), 1)
             cv2.putText(final_img, f"type: {self.type}, marker_id: {next_aruco_id}", (10,60), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,0,255), 1)
             cv2.imshow("Original Image", final_img)
             cv2.waitKey(1)
         self.cmd_vel_pub.publish(cmd_vel)
 
     def advanced_control_callback(self, data):
+        if self.path is not None:
+            rospy.loginfo("Already in path")
+            return
+
         if data is None:
             return
 
         self.cmd = data.cmd
-        self.dest = data.dest
+        self.dest = int(data.dest)
 
-        # dest에 따른 path 설정 => 현재 6번셀 하드코딩 되어있다.
-        self.path = {
-            1 : {"next_aruco_id": 3, "lane_color": "green", "turn": "s"},
-            3 : {"next_aruco_id": 4, "lane_color": "green", "turn": "r"},
-            4 : {"next_aruco_id": 6, "lane_color": "green", "turn": "l"}
-        }
-        self.current_aruco_id = 1
+        self.path = self.get_path(self.current_aruco_id, self.dest)
+        if self.path is None:
+            rospy.logerr("Failed to get path")
+            return
+
+        # transform keys to int
+        self.path = {int(k):v for k,v in self.path.items()}
+
         self.type = 3 # turn
 
     def clean_up(self):
